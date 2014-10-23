cea93bdfd94d70a2bd09c493715c6cac6519e902
Joseph Benden <joe@benden.us>
2014-10-23 14:24:09 -0700

    Verify that power state modes are indeed active.

--- pmset_test.js
+++ pmset_test.js
@@ -1,5 +1,6 @@
 process.env.NODE_ENV = "test";
 var pmset = require('./pmset');
+var exec = require('child_process').exec;
 
 function ok(expr, msg) {
       if (!expr) throw new Error(msg);
@@ -9,14 +10,30 @@ describe("Power Management functions for OS X", function(done) {
     it("Should be an integer value greater than 0. #1", function (done) {
         id = pmset.noDisplaySleep("Testing Node.js");
         ok(id > 0);
-        pmset.release(id);
-        done();
+        var found = false;
+        exec('pmset -g assertions', function (error, stdout, stderr) {
+            var regex = /NoDisplaySleepAssertion named: .Testing Node\.js./g;
+            while (match = regex.exec(stdout)) {
+                found = true;
+            }
+            ok(found == true);
+            pmset.release(id);
+            done();
+        });
     });
     it("Should be an integer value greater than 0. #2", function (done) {
         id = pmset.noIdleSleep("Testing Node.js");
         ok(id > 0);
-        pmset.release(id);
-        done();
+        var found = false;
+        exec('pmset -g assertions', function (error, stdout, stderr) {
+            var regex = /NoIdleSleepAssertion named: .Testing Node\.js./g;
+            while (match = regex.exec(stdout)) {
+                found = true;
+            }
+            ok(found == true);
+            pmset.release(id);
+            done();
+        });
     });
 });
 
