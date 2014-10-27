6c64d42eb6e3100b43e9023216ccea440f52e149
Joseph Benden <joe@benden.us>
2014-10-27 08:03:47 -0700

    Bump.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,5 @@
 module.exports = function(grunt) {
-
     grunt.loadNpmTasks('grunt-mocha-test');
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         mochaTest: {
@@ -14,6 +12,5 @@ module.exports = function(grunt) {
             }
         }
     });
-
     grunt.registerTask('default', ['mochaTest']);
 };
